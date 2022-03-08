7a076f0df80b3c7285edc31a9c5446472360e3b0
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-03-08 16:01:06 -0500

    app preview

--- src/main/java/com/api/appdogapp/AppdogappApplication.java
+++ src/main/java/com/api/appdogapp/AppdogappApplication.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-public class AppdogappApplication {
+public class AppdogappApplication implements WebMvcConfigurer{
     @Autowired
     public MascotaRepository repository;
 
@@ -35,6 +37,13 @@ public class AppdogappApplication {
         System.out.println(mascota.toString());
         repository.deleteById(mascota.getId());
         return repository.save(mascota);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/health")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 
     public static void main(String[] args) {
