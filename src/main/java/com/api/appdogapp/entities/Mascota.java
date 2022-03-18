279fc9f9d53d2592cf6cc8940d5002a369c3fc7e
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-03-18 01:22:30 -0500

    version 2

--- src/main/java/com/api/appdogapp/entities/Mascota.java
+++ src/main/java/com/api/appdogapp/entities/Mascota.java
@@ -3,17 +3,8 @@ package com.api.appdogapp.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Table
 public class Mascota {
     @Id
     @GeneratedValue
